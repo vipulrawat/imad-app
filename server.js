b1afdf1aa9c9ce25440d9c77311997f1bd220eef
Vipul <vipulrawat.imi@live.in>
2017-08-18 21:51:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pool').Pool;
+
+var config = {
+    user: 'vipulrawat007',
+    database:'vipulrawat007',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process_env.DB_PASSWORD
+    
+};
+var pool = new Pool(config);
+
 
 var app = express();
 app.use(morgan('combined'));
 app.use('/ui',express.static('ui'));
+
+app.get('/dbtest',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err)
+            res.status(500).send(err.toString());
+        else
+            res.send(JSON.stringify(result));
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
