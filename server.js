978da21804619ee3a6a84321573a9fdac8c23e6d
Vipul <vipulrawat.imi@live.in>
2017-08-18 21:58:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-const { Pool, Client } = require('pg');
+const Pool= require('pg');
 
 const pool = new Pool({
   user: 'vipulrawat007',
