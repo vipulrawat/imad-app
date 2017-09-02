829da267905d1a00c11d9696a6d1903c61a22b69
Vipul <vipulrawat.imi@live.in>
2017-09-02 18:28:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -54,8 +54,7 @@ app.get('/createUser',function(req,res){
     var dbString = hash(password,slt);
     pool.query('INSERT INTO "users" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
-             //res.status(500).send(err.toString());
-             res.send("failed");
+             res.status(500).send(err.toString());
          }else{
              res.send('User successfully added : '+username);
          }
